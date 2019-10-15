bd994d920845abd5aaf010d7569b253987c30b64
AislingOL <55489818+AislingOL@users.noreply.github.com>
2019-10-15 09:55:03 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The fllowing functions create an object that can cache the inverse of a matrix, and then retreive the inverse from the cache.
 
-## Write a short comment describing this function
+## This function creates a"matrix" object capable of caching its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+        x <<- y
+        m <<- NULL
+  }
+  get <- function() x
+  set_inverse_matrix <- function(inverse) m <<- inverse
+  get_inverse_matrix <- function() m
+  list(set = set, get = get,
+      set_inverse_matrix = set_inverse_matrix,
+      get_inverse_matrix = get_inverse_matrix)
+  }
 
-}
 
-
-## Write a short comment describing this function
+##  This function computes the inverse of the "matrix" returned by makeCacheMatrix above. 
+## If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$get_inverse_matrix()
+  if(!is.null(m)) {
+         message("getting cached data")
+         return(m)
+  }
+     data <- x$get()
+     m <- solve(data, ...)
+     x$set_inverse_matrix(m)
+     m
+   }
